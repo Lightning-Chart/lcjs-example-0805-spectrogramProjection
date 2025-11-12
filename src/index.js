48e78a68a5953a2143ee4c2c8d1a9e9a18001a3c
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2025-11-12 09:26:36 +0200

    LCJS v8.1.0

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const spectrogramRows = 1024
 
 // Create charts and series.
 
-// NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
+// NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/more-guides/grouping-charts/
 const dashboard = lightningChart({
             resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
         })
